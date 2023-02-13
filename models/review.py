bef3445dc38cc5ae4ce6b2b7a930bcd32a8133b8
Chidera2468 <chidera2468>
2023-02-13 11:18:49 -0800

    from

--- models/review.py
+++ models/review.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from sqlalchemy.sql.schema import ForeignKey
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from models import storage_type
+
+"""This file defines the Review Model
+It inherits from the BaseModel
+"""
+
+from models.base_model import BaseModel
 
 
-class Review(BaseModel, Base):
-    """ Review classto store review information """
-    __tablename__ = 'reviews'
-    if storage_type == 'db':
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+class Review(BaseModel):
+    """The Review Model"""
+
+    # Attributes
+    place_id: str = ""
+    user_id: str = ""
+    text: str = ""
