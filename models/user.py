b4e7dffb9b49a425de5f054a0b56aea4994a828e
Chidera2468 <chidera2468>
2023-02-13 11:25:25 -0800

    show rm git commit -m show

--- models/user.py
+++ models/user.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
-from models import storage_type
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+
+"""This file defines the UserModel class
+It inherits from the BaseModel
+"""
+
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    if storage_type == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+class User(BaseModel):
+    """The User Model"""
+
+    # Attributes
+    email: str = ""
+    password: str = ""
+    first_name: str = ""
+    last_name: str = ""
